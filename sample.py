f151f0a7dbcebbc3228c37f90ad36262ee002439
Shantanu-Bakshi <72117343+Shantanu-Bakshi@users.noreply.github.com>
2021-04-22 16:01:06 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,13 +1,8 @@
 def sort_list(list1, list2):
- 
     zipped_pairs = zip(list2, list1)
- 
     z = [x for _, x in sorted(zipped_pairs)]
-     
     return z
      
- 
-
 x = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 y = [ 0,   1,   1,    0,   1,   2,   2,   0,   1]
  
